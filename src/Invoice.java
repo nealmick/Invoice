16176ee0783c7cea1ad4a51fdf6269caececc008
Neal <33704976+nealmick@users.noreply.github.com>
2022-09-24 14:16:15 -0400

    Add files via upload

---
+++ src/Invoice.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author neal
+ */
+public class Invoice {
+    private String customerType;
+    private double subtotal;
+   
+    public Invoice(String customerType, double subtotal){
+        
+    }
+    public int get()
+    {
+        
+        return;
+    }
+    
+    
+    
+}
